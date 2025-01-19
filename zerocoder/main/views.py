fce0890123ea54ce9079b1575559fb8f50650279
obogrelova <on.obogrelova@gmail.com>
2025-01-19 11:38:43 +0500

    new files

---
+++ zerocoder/main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def data(request):
+    return HttpResponse("<h1>Это мой первый проект на Django.</h1>")
+
+def test(request):
+    return HttpResponse("<h1>Это вторая страница моего проекта на Django.</h1>")
